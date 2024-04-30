474fbc2c6e5d08c36d4df1f7e920216e24fe181f
GoodErnest64 <97891253+GoodErnest64@users.noreply.github.com>
2024-04-30 12:03:47 +0330

    C++

--- RNG.cpp
+++ RNG.cpp
@@ -13,7 +13,7 @@ const int MAX = 100;
 int main()
 {
     srand(time(0));
-    num = (rand() % (MAX - MIN + 1)) + MIN;
+    num = (rand() % (MAX - MIN + 1)) + MIN; ;
     while (1){
         cout << "Please Enter Your Guess : ";
         //cin >> guess;
@@ -34,7 +34,7 @@ int main()
             cout<<("Your Guess Is Too Low !\n");
         }
         else if(guess == num){
-            cout<<("You Won ! The Number Was %d And You Guessed It In %d Guesses !\n", num, guesses);
+            cout<<"You Won ! The Number Was "<<num<<" And You Guessed It In "<<guesses<<" Guesses !\n";
             break;
         }
         guesses++;
